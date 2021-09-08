bd48e9eaa65a9f72f820cd0f7a66426b05e51432
son00326 <90251739+son00326@users.noreply.github.com>
2021-09-08 16:16:26 +0900

    Add files via upload

---
+++ Calcurator.java
@@ -0,0 +1,57 @@
+package com.javalec.function;
+
+public class Calcurator {
+
+	
+	
+	//Field and property 
+	int i = 0; 
+	int k = 0;
+	
+	public Calcurator(int i, int k) {
+		super();
+		this.i = i;
+		this.k = k;
+	}
+	
+	
+	//Constructor
+	public Calcurator() {
+		
+	}
+	
+	//method 
+	public void calc() { 
+		int sum = i + k  ; 
+		System.out.println("덧셈 : " + sum);
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
